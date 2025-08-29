6c6075dc4489c4907251cb7c9ba3e046bb7f292d
mimi2456 <mimi24cz@gmail.com>
2025-08-29 14:16:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const img = document.getElementById("img")
 const button = document.getElementById("button")
 
+let JSON_URL = "";
 
 function ChanMode() {
-   let JSON_URL = "Chan.json";
+    JSON_URL = "Chan.json";
     fetchJSON(JSON_URL);
 }
 
 function ChangbinMode() {
-   let JSON_URL = "Changbin.json";
+    JSON_URL = "Changbin.json";
     fetchJSON(JSON_URL);
 }
 
 function Allmode() {
-   let JSON_URL = "All.json";
+    JSON_URL = "All.json";
     fetchJSON(JSON_URL);
 }
 
